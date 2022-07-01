a5a92b642b933198f7cf0bb277da9f16741c1ae6
mimimia0345 <108380926+mimimia0345@users.noreply.github.com>
2022-07-01 16:55:27 +0900

    Update RepositorySearchesProcessor.java
    com.example.petstore.

--- bidding-domain/src/main/java/com/example/petstore/RepositorySearchesProcessor.java
+++ bidding-domain/src/main/java/com/example/petstore/RepositorySearchesProcessor.java
@@ -9,14 +9,14 @@ import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RepositorySearchesProcessor implements RepresentationModelProcessor<EntityModel<Pet>> {
+public class RepositorySearchesProcessor implements RepresentationModelProcessor<EntityModel<Bidding>> {
 
     @Override
-    public EntityModel<Pet> process(EntityModel<Pet> model) {
-        model.add(Link.of(model.getRequiredLink("self").getHref() + "/feed").withRel("feed"));
+    public EntityModel<Bidding> process(EntityModel<Bidding> model) {
+        // model.add(Link.of(model.getRequiredLink("self").getHref() + "/feed").withRel("feed"));
 
-        if(model.getContent() instanceof Cat)
-            model.add(Link.of(model.getRequiredLink("self").getHref() + "/groom").withRel("groom"));
+        // if(model.getContent() instanceof Cat)
+        //    model.add(Link.of(model.getRequiredLink("self").getHref() + "/groom").withRel("groom"));
 
         return model;
     }
