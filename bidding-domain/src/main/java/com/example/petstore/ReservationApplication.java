9b9d77404362a009227f60efdaf389ceece2e781
mimimia0345 <108380926+mimimia0345@users.noreply.github.com>
2022-07-01 17:28:25 +0900

    Update ReservationApplication.java

--- bidding-domain/src/main/java/com/example/petstore/ReservationApplication.java
+++ bidding-domain/src/main/java/com/example/petstore/ReservationApplication.java
@@ -3,13 +3,13 @@ package com.example.petstore;
 import com.example.petstore.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+// import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
-@EnableFeignClients
+// @EnableFeignClients
 public class ReservationApplication {
 
     public static ApplicationContext applicationContext;
