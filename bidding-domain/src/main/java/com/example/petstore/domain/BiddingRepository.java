9943377b39d91c350bfa49822f5ac3e4e110a39b
mimimia0345 <108380926+mimimia0345@users.noreply.github.com>
2022-07-01 16:41:15 +0900

    Update BiddingRepository.java
    com.example.petstore.

--- bidding-domain/src/main/java/com/example/petstore/domain/BiddingRepository.java
+++ bidding-domain/src/main/java/com/example/petstore/domain/BiddingRepository.java
@@ -1,6 +1,6 @@
-package biddinglv.domain;
+package com.example.petstore.domain;
 
-import biddinglv.domain.*;
+import com.example.petstore.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
