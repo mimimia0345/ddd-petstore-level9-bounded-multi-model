166ad97ebbbd95deec2b86bd6895d8fbe396dacf
mimimia0345 <108380926+mimimia0345@users.noreply.github.com>
2022-07-01 17:32:10 +0900

    Update PolicyHandler.java

--- bidding-domain/src/main/java/com/example/petstore/PolicyHandler.java
+++ bidding-domain/src/main/java/com/example/petstore/PolicyHandler.java
@@ -1,19 +1,29 @@
 package com.example.petstore;
 
 import com.example.petstore.kafka.KafkaProcessor;
+import com.example.petstore.domain.*;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.naming.NameParser;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PolicyHandler{
+@Transactional
+public class PolicyHandler {
 
+    @Autowired
+    BiddingRepository biddingRepository;
+
+    //@Autowired
+    //GameReservationRepository gameReservationRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void whatever(@Payload String eventString){}
-
-
-
+    public void whatever(@Payload String eventString) {}
+    // keep
 
 }
